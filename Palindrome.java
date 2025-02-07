e62bdf34a9e93dc3d49e22a753c7a6d735253c9e
GaoMing <gaoming@gmail.com>
2025-02-07 12:46:40 +0700

    Update main funtion

--- Palindrome.java
+++ Palindrome.java
@@ -36,15 +36,15 @@ public class Palindrome {
         System.out.println("Enter your string: ");
         String str = SC.nextLine().trim();
 
-        System.out.println("The number of words in the string is: " + DoDaiChuoi(str));
-        System.out.println("The number of characters in the string (excluding spaces) is: " + DemTu(str));
-        KiemTraChuoiDoiXung(str);
+        System.out.println("The number of words in the string is: " + countWords(str));
+        System.out.println("The number of characters in the string (excluding spaces) is: " + countCharacters(str));
+        checkPalindrome(str);
 
         System.out.println("Enter your first string to concatenate: ");
         String str1 = SC.nextLine().trim();
         System.out.println("Enter your second string to concatenate: ");
         String str2 = SC.nextLine().trim();
-        System.out.println("The concatenated string is: " + NoiChuoi(str1, str2));
+        System.out.println("The concatenated string is: " + concatenateStrings(str1, str2));
 
         SC.close();
     }
